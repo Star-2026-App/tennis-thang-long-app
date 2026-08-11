03c8136f5f3b41d1f08fbabc815795adf63b66f5
Star-2026-App <saothaiduongdt@gmail.com>
2026-08-11 10:09:41 +0700

    Refactor Phase 1 - Move config out of app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzfqbFiXKuRa9cLvhJvPXR9dnGLmIrasfKTXhJmbyz1mS3Eivu5fJZs4hk99l2dbMXtg/exec";
 
 let currentUserRole = "member";
 let loggedInMemberName = "";
