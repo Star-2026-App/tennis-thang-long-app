f23e35fd0e5a8f67bff134221b9bd49a90b6c44c
Star-2026-App <saothaiduongdt@gmail.com>
2026-08-12 12:54:47 +0700

    Connect quyLogs preview to TEST backend

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzzfqbFiXKuRa9cLvhJvPXR9dnGLmIrasfKTXhJmbyz1mS3Eivu5fJZs4hk99l2dbMXtg/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyNBnQ8xb62er_g4XSV9TQjKN3A0H8uaC5jB_P3BwiRg4ROsobUOdO1HzfvB6Jmis0wkQ/exec";
