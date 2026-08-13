72d4fc29f66d6d9c2ff5007739bd27f0eeb86519
Star-2026-App <saothaiduongdt@gmail.com>
2026-08-13 11:44:37 +0700

    Add Vercel proxy for Google Apps Script

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyZf5YYKH6x60cYNXEI1Xsrd0JAyscB0r0fDMItspvJYOqUG6Hu2Thg9e86MzkR2VwFTQ/exec";
+const GOOGLE_SCRIPT_URL = "/api/google-script";
