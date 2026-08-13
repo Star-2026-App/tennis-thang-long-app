e0b29a5792ec5dab7262f33c15d40f42cb7eeb6e
Star-2026-App <saothaiduongdt@gmail.com>
2026-08-13 11:32:30 +0700

    Point frontend to Backend TEST V13

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyNBnQ8xb62er_g4XSV9TQjKN3A0H8uaC5jB_P3BwiRg4ROsobUOdO1HzfvB6Jmis0wkQ/exec";
+https://script.google.com/macros/s/AKfycbyZf5YYKH6x60cYNXEI1XsrdOJAyscB0rOfDMltspvJYOqUG6Hu2Thg9e86MzkR2VwFTQ/exec
