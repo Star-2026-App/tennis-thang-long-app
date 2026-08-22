78928d16dcd270c146dcc1f6da8039f3aa2fa466
Star-2026-App <saothaiduongdt@gmail.com>
2026-08-22 08:24:46 +0700

    Update config API Token

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@ const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwbPZaEG-qqUB
 // Script Properties). Đổi giá trị bên dưới thì cũng phải
 // đổi lại giá trị API_SECRET tương ứng ở backend.
 // ======================================================
-const API_TOKEN = "3cafb385ebdafaf321d324cfc0ccbf1083ff99";
+const API_TOKEN = "e330c8170c3cafb385ebdafaf321d324cfc0ccbf1083ff99";
